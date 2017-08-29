de4d0891f6133f8078100596b53ab4d6394bf726
Gwilym Rowlands <gwilym.rowlands@gmail.com>
2017-08-29 18:48:42 +0100

    Not sure why

---
+++ Web1/www/index.php
@@ -0,0 +1,12 @@
+<H1>Apache Docker Web Page - Server 1 v1</H1>
+<?php
+echo "Server Name: ", $_SERVER['SERVER_NAME'], " <P>";
+echo "Server Addr: ", $_SERVER['SERVER_ADDR'], " <P>";
+echo "Server Port: ", $_SERVER['SERVER_PORT'], " <P>";
+echo "Hostname: ", exec('hostname'), " <P>";
+echo '<P>';
+
+#echo '<P>';
+#phpinfo();
+
+?>
