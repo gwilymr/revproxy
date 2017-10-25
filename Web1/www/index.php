708cd1871ab0a8747f42a6b048df662ff7ef7e3d
gwilymr <31447106+gwilymr@users.noreply.github.com>
2017-10-25 19:20:13 +0100

    Update index.php
    Added additional call to get IP details.

--- Web1/www/index.php
+++ Web1/www/index.php
@@ -4,6 +4,7 @@ echo "Server Name: ", $_SERVER['SERVER_NAME'], " <P>";
 echo "Server Addr: ", $_SERVER['SERVER_ADDR'], " <P>";
 echo "Server Port: ", $_SERVER['SERVER_PORT'], " <P>";
 echo "Hostname: ", exec('hostname'), " <P>";
+echo "All IPs:  ", exec('hostname -I'), " <P>";
 echo '<P>';
 
 #echo '<P>';
